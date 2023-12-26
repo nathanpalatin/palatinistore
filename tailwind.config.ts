0ca6fc9acec3210de1ec9b664200d722cc4b6188
Nathan Palatin <nath.palatin@gmail.com>
2023-12-26 09:24:06 -0300

    add theme schema

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'tailwindcss'
 
 const config = {
   content: [
-    './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
